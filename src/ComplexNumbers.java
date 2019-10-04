5846e02961804f4e13d1078d16d8c738422897f3
gerardjaque.agravant <gerardjaque.agravant@zone39-wc.cs.ucalgary.ca>
2019-10-04 12:39:05 -0600

    Don't open link

--- src/ComplexNumbers.java
+++ src/ComplexNumbers.java
@@ -1,6 +1,6 @@
 
 public class ComplexNumbers {
 	public static void main (String[] args) {
-		System.out.println("Hi master");
+		System.out.println("https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/03/Maine-Coon_02.jpg");
 	}
 }
