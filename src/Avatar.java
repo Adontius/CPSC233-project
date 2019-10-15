3860f8404e9b218017fff185f798ef6c880b4d0e
alicenormanid <56169920+alicenormanid@users.noreply.github.com>
2019-10-15 09:49:43 -0600

    Update Avatar.java

--- src/Avatar.java
+++ src/Avatar.java
@@ -1,12 +1,14 @@
 //This class holds the variables related to the avatar, including tip money count, strike count, vertical and horizontal position, and whether
 //the avatar has delivered pizza or not. It also has movement methods (move up, down, left, or right). Each variable has it's respective setter 
 //and getter methods.
+//author @alice
+
 public class Avatar
 {
-    public int vertPos; //vertical position of avatar (y axis)
     private int tipMoney; //counter for amount of tip money the avatar has accumulated. Has setter and getter.
     public int strikeCount; //counter for number of strikes the avatar has accummulated. Has setter and getter.
     public int vertPos; //vertical position of avatar (y axis). Has setter and getter.
+    public int horzPos; //horizontal position of avatar (x axis). Has setter and getter.
     public boolean pizzaDelivered; //booelan to store whether pizza has been delivered (true) or not (false). Has setter and getter.
 
     public Avatar()
@@ -21,23 +23,35 @@ public class Avatar
 
     public void setVertPos(int vertPos)//sets avatar's vertical position
     {
+        this.vertPos = vertPos;
     }
 
     public int getVertPos()//returns the vertical position of the avatar
     {
         return this.vertPos;
     }
+
+    public void setHorzPos(int horzPos)//sets the avatar's horizontal position
+    {
+        this.horzPos = horzPos;
+    }
+
+    public int getHorzPos()//returns the horizontal position of the avatar
+    {
+        return this.horzPos;
+    }
+
     public void moveUp() //moves avatar one unit up (y + 1)
     {
         vertPos += 1;
     }
 
-    public void moveDown()
+    public void moveDown() //moves avatar one unit down (y - 1)
     {
         vertPos -= 1;
     }
 
-    public void moveRight()
+    public void moveRight()//moves avatar one unit right (x + 1)
     {
         horzPos += 1;
     }
@@ -47,7 +61,7 @@ public class Avatar
         horzPos -= 1;
     }
 
-    public boolean deliverPizza()
+    public boolean deliverPizza() //when invoked, sets pizzaDelivered to true to signal that a pizza has been delivered. No count of pizzas required
     {
         pizzaDelivered = true;
         return this.pizzaDelivered;
