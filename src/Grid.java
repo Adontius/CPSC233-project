12e4a9bee12e949ca10a2591a15b30af77dd84c0
Adontius <55174009+Adontius@users.noreply.github.com>
2019-10-16 20:43:06 -0600

    Grid class

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,31 @@
 
 public class Grid {
+    private int PositionNumber;
+    private char symbol;
+
+    public int getPositionNumber(){
+        return PositionNumber;
+    }
+
+    public char getSymbol(){
+        return symbol;
+    }
+
+    public int setPositionNumber(int p){
+        PositionNumber = p;
+    }
+
+    public char setSymbol(char s){
+        symbol = s;
+    }
+
+}
+
+public class House extends Grid{
+    private boolean hasOrder;
+
+}
+
+public class Obstacles{
 
 }
