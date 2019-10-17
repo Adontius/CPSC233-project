921fe8823ac3dad56dd81e2c711c70450b10ca3b
warayUpayGud <gerardagravante@gmail.com>
2019-10-17 04:26:13 -0600

    I changed some stuff on the Grid class cause it was giving errors

--- src/Grid.java
+++ src/Grid.java
@@ -11,41 +11,11 @@ public class Grid {
         return symbol;
     }
 
-    public int setPositionNumber(int p){
+    public void setPositionNumber(int p){
         int PositionNumber = p;
     }
 
-    public char setSymbol(char s){
+    public void setSymbol(char s){
         char symbol = 's';
-    }
-}
-
-public class House extends Grid{
-    private boolean order;
-
-    public House(boolean order) {
-        order = true;
-    }
-
-    public boolean hasOrder(){
-        return this.order;
-    }
-
-    public boolean setOrder(boolean order){
-        return this.order = order;
-    }
-
-}
-
-public class Obstacles{
-    private boolean activated;
-    private int obstacles;
-
-    public boolean checkObstacles(){
-        return this.activated;
-    }
-
-    public boolean setObstacles(boolean obstacles){
-        return this.activated = activated;
     }
 }
