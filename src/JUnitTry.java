ff5a515509fb19208660a1cf261b943d67f20836
Khushi Bhatt <khushi.bhatt1@ucalgary.ca>
2019-11-12 14:33:59 -0700

    JUnit Test?

---
+++ src/JUnitTry.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JUnitTry {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
