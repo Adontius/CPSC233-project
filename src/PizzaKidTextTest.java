acec5e3579f280a9a2b01e2077ebdcfcf37db282
Khushi Bhatt <khushi.bhatt1@ucalgary.ca>
2019-11-21 03:39:27 -0700

    JUnit skeleton

---
+++ src/PizzaKidTextTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PizzaKidTextTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPlay() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testShowStartScreen() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testShowPlayScreen() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDisplayMapToConsole() {
+		fail("Not yet implemented"); // TODO 
+	}
+
+}
