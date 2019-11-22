8b91ec8b44e6278a45e3937584e71c959f657466
alicenormanid <56169920+alicenormanid@users.noreply.github.com>
2019-11-22 12:35:09 -0700

    JUnit tests are working!

--- src/PizzaKidTextTest.java
+++ src/PizzaKidTextTest.java
@@ -14,28 +14,63 @@ import static org.junit.runners.Parameterized.*;
 public class PizzaKidTextTest {
 
 	@Test
-	public final void testMain() {
-		fail("Not yet implemented"); // TODO
+	public final void testCheckSurroundings_customerAbove() 
+	{
+		Avatar p = new Avatar();
+		Tile[][] tile = new Tile[5][5];
+		p.setCol(2);
+		p.setRow(2);
+		tile[1][2] = new Customer();
+		
+		PizzaKid pk = new PizzaKid();
+		pk.map = new Map(p, 5, tile);		
+		
+		assertEquals("Checked if tile above player is a customer.", true, pk.checkSurroundings(1) instanceof Customer);
 	}
 
 	@Test
-	public final void testPlay() {
-		fail("Not yet implemented"); // TODO
+	public final void testCheckSurroundings_customerBelow() 
+	{
+		Avatar p = new Avatar();
+		Tile[][] tile = new Tile[5][5];
+		p.setCol(2);
+		p.setRow(2);
+		tile[3][2] = new Customer();
+		
+		PizzaKid pk = new PizzaKid();
+		pk.map = new Map(p, 5, tile);		
+		
+		assertEquals("Checked if tile below player is a customer.", true, pk.checkSurroundings(3) instanceof Customer);
 	}
 
 	@Test
-	public final void testShowStartScreen() {
-		fail("Not yet implemented"); // TODO
+	public final void testCheckSurroundings_customerLeft() 
+	{
+		Avatar p = new Avatar();
+		Tile[][] tile = new Tile[5][5];
+		p.setCol(2);
+		p.setRow(2);
+		tile[2][1] = new Customer();
+		
+		PizzaKid pk = new PizzaKid();
+		pk.map = new Map(p, 5, tile);		
+		
+		assertEquals("Checked if tile to left is a customer.", true, pk.checkSurroundings(2) instanceof Customer);
 	}
 
 	@Test
-	public final void testShowPlayScreen() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testDisplayMapToConsole() {
-		fail("Not yet implemented"); // TODO
+	public final void testCheckSurroundings_customerRight() 
+	{
+		Avatar p = new Avatar();
+		Tile[][] tile = new Tile[5][5];
+		p.setCol(2);
+		p.setRow(2);
+		tile[2][3] = new Customer();
+		
+		PizzaKid pk = new PizzaKid();
+		pk.map = new Map(p, 5, tile);		
+		
+		assertEquals("Checked if tile to right is a customer.", true, pk.checkSurroundings(4) instanceof Customer);
 	}
 
 	// help 
