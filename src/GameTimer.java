e0a6f240846659997ad57e1fd4dec7c979497722
alicenormanid <56169920+alicenormanid@users.noreply.github.com>
2019-11-28 10:17:10 -0700

    Game timer edits

--- src/GameTimer.java
+++ src/GameTimer.java
@@ -19,42 +19,12 @@ import javafx.util.Duration;
 
 public class GameTimer extends AnimationTimer
 {
-	//constant to indicated how many seconds the timer counts down from
-	private final int START_TIME = 60;
-	
-	private Label timerLabel = new Label();
-	private int timeSeconds = START_TIME;
+
 	
 	public static void main(String[] args) 
 	{
 		Application.launch(args);
 	}
 	
-	public void start(Stage primaryStage)
-	{
-		//Setup stage and scene
-		primaryStage.setTitle("PizzaKid Timer");
-		Group root = new Group();
-		Scene scene = new Scene(root, 300, 300);
-		
-		//Details of the timer label
-		//timerLabel.setText();
-		timerLabel.setTextFill(Color.INDIANRED);
-		
-	}
 
-	@Override
-	public void handle(long now) {
-		
-	}
-	
-//	AnimationTimer timer = new AnimationTimer() {
-//
-//		@Override
-//		public void handle(long arg0) {
-//			map.showGUIMap(mapGUI);
-//		}
-//
-//	};
-//	timer.start();
 }
