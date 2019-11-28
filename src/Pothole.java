aed0d1d574a309f075bd407ce16cae87b48a6952
alicenormanid <56169920+alicenormanid@users.noreply.github.com>
2019-11-28 10:27:09 -0700

    edits

--- src/Pothole.java
+++ src/Pothole.java
@@ -1,4 +1,3 @@
-
 /**Potholes are a type of Obstacle.
  * They are randomly generated on the Map, and respawn in a new random location once the player has ran into them.
  * Running into a Pothole gives the player a strike and the player is relocated to the starting location.
